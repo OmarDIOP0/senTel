89fcf1cbfb1c647298e631d2ab4ebe8d8f71c80f
OmarDIOP0 <odiop6170@gmail.com>
2025-07-07 23:21:56 +0000

    contexte authentification

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -6,15 +6,15 @@ export default AuthContext;
 
 
 export const AuthProvider =({children}) =>{
-    const [authTokens, setAuthTokens] = useState(()=>
-        localStorage.getItem("authTokens")
-        ? JSON.parse(localStorage.getItem("authTokens"))
+    const [token, setAuthTokens] = useState(()=>
+        localStorage.getItem("token")
+        ? JSON.parse(localStorage.getItem("token"))
         : null
     );
 
     const [user, setUser] = useState(()=>
-        localStorage.getItem("authTokens")
-       ? jwtDecode(localStorage.getItem('authTokens'))
+        localStorage.getItem("token")
+       ? jwtDecode(localStorage.getItem('token'))
        : null
 );
     const [loading,setLoading] =useState(true);
@@ -24,20 +24,21 @@ export const AuthProvider =({children}) =>{
 
     const loginMutation = useMutation({
         mutationFn: async ({email, password}) =>{
-            const response = await axios.post(`${APIURL}/token/`,{email, password});
+            const response = await axios.post(`${APIURL}/admin/login`,{email, password});
             return response.data;
         },
         onSuccess: (data) => {
             setAuthTokens(data);
             const decodedToken = jwtDecode(data.access);
             setUser(decodedToken);
-            localStorage.setItem('authTokens', JSON.stringify(data));
+            localStorage.setItem('token', JSON.stringify(data));
             // Redirection vers le tableau de bord
-            if (decodedToken.role === 'student') {
-                navigate('/');
+            if (decodedToken.role === 'ADMIN') {
+                navigate('/dashboard/admin?sucess=true&&redirect=true');
             }
             else {
-                navigate('/dashboard/teacher?sucess=true&&redirect=true');
+                navigate('/login');
+                return;
             }
             // navigate('/dashboard');
             showAlert("Login Success🚀✅", "success");
@@ -68,18 +69,30 @@ export const AuthProvider =({children}) =>{
     });
     // Mutation pour l'inscription
     const registerMutation = useMutation({
-        mutationFn: async ({ email,role, password, confirm_password }) => {
-          const response = await axios.post(`${APIURL}/register/`, {
+        mutationFn: async ({ nomComplet,email,role, password  }) => {
+          const response = await axios.post(`${APIURL}/auth/register`, {
+            nomComplet,
             email,
             role,
-            password,
-            confirm_password,
+            password
           });
           return response.data;
         },
         onSuccess: () => {
-          navigate("/login");
-          showAlert("Registration Success🚀✅", "success");
+           setAuthTokens(data);
+            const decodedToken = jwtDecode(data.access);
+            setUser(decodedToken);
+            localStorage.setItem('token', JSON.stringify(data));
+            // Redirection vers le tableau de bord
+            if (decodedToken.role === 'ADMIN') {
+                navigate('/dashboard/admin?sucess=true&&redirect=true');
+            }
+            else {
+                navigate('/login');
+                return;
+            }
+            // navigate('/dashboard');
+            showAlert("Register Success🚀✅", "success");
         },
         onError: (error) => {
           let messageError = "Une erreur est survenue";
@@ -105,87 +118,16 @@ export const AuthProvider =({children}) =>{
           });
         },
       });
-    // Mutation pour google
-    const googleLoginMutation = useMutation({
-      mutationFn: async(credential) =>{
-        const response = await axios.post(`${APIURL}/login/google/`, {
-          token: credential,
-        });
-        return response.data;
-      },
-      onSuccess:(data) =>{
-        setAuthTokens(data);
-        setUser(jwtDecode(data.access));
-        localStorage.setItem('authTokens', JSON.stringify(data));
-        navigate('/dashboard');
-        showAlert("Google Registration Success 🚀✅", "success");
-      },
-      onError: (error) => {
-        // showAlert("Google Authentication Failed ❌", "error");
-        let messageError = "Une erreur est survenue";
-        if(error.response && error.response.data){
-          if(typeof error.response.data === "string"){
-            messageError = error.response.data;
-          }else if(typeof error.response.data === "object"){
-            messageError = Object.values(error.response.data).flat().join("\n");
-          }
-        }
-        swal.fire({
-          title: `Google Authentication Failed ❌${messageError}`,
-          icon: "error",
-          toast: true,
-          timer: 6000,
-          position: "top-right",
-          timerProgressBar: true,
-          showConfirmButton: false,
-          width: "auto", 
-            customClass: {
-              popup: 'swal2-toast', 
-            }
-        });
-    }
-    });
-    const handleGoogleSuccess = (response) => {
-      const { credential } = response;
-      if (!credential) {
-        showAlert("Google Authentication Failed ❌: No credential found", "error");
-        return;
-      }
-      googleLoginMutation.mutate({token:credential});
-  };
-  const handleGoogleFailure = () => {
-    showAlert("Google Authentication Failed ❌", "error");
-};
 
-        // Fonction de déconnexion
     const logoutUser = () => {
         setAuthTokens(null);
         setUser(null);
-        localStorage.removeItem("authTokens");
+        localStorage.removeItem("token");
         navigate("/login");
         showAlert("You have been logged out 🫡", "success");
     };
-    const refreshToken = useMutation({
-      mutationFn: async () => {
-        const response = await axios.post(`${APIURL}/token/refresh/`, {
-          refresh: authTokens.refresh,
-        });
-        return response.data;
-      },
-      onSuccess: (data) => {
-        setAuthTokens(data);
-        setUser(jwtDecode(data.access));
-        localStorage.setItem('authTokens', JSON.stringify(data));
-      },
-      onError: (error) => {
-        console.error("Erreur lors du refresh du token", error);
-        logoutUser();
-      },
-    })
-
-    // Donnees du contexte
     const contextData = {
-        authTokens,
+        token,
         user,
         loginMutation,
         registerMutation,
@@ -193,23 +135,8 @@ export const AuthProvider =({children}) =>{
         loading,
         setUser,
         setAuthTokens,
-        refreshToken,
-        handleGoogleSuccess,
-        handleGoogleFailure,
-        googleLoginMutation
     };
 
-    // Effet pour decoder le token au chargement
-    useEffect(()=>{
-        if(authTokens){
-            setUser(jwtDecode(authTokens.access));
-            const refreshinterval = setInterval(()=>{
-              refreshToken.mutate();
-            },3000000);
-
-            return () => clearInterval(refreshinterval);
-          }
-    }, [authTokens,loading]);
 
     return (
         <AuthContext.Provider value={contextData}>
