bdde32e9dcc17cc82e21f9eac690dc3314aba43b
OmarDIOP0 <odiop6170@gmail.com>
2025-07-09 22:09:35 +0000

    service projet

---
+++ frontend/utils/projetService.js
@@ -0,0 +1,26 @@
+import { authorizedApi } from "./api";
+
+export const getAllProjets = async () => {
+  const response = await authorizedApi().get("/projet");
+  return response.data;
+};
+
+export const getProjetById = async (id) => {
+  const response = await authorizedApi().get(`/projet/${id}`);
+  return response.data;
+};
+
+export const createProjet = async (data) => {
+  const response = await authorizedApi().post("/projet", data);
+  return response.data;
+};
+
+export const updateProjet = async (data) => {
+  const response = await authorizedApi().put("/projet/", data);
+  return response.data;
+};
+
+export const deleteProjet = async (id) => {
+  const response = await authorizedApi().delete(`/projet/${id}`);
+  return response.data;
+};
