e27fa9ff9fd94232ea0d53ddd60e392d28d1e09d
OmarDIOP0 <odiop6170@gmail.com>
2025-07-09 21:18:18 +0000

    service configuration

---
+++ frontend/utils/configurationService.js
@@ -0,0 +1,26 @@
+import { authorizedApi } from "./api";
+
+export const getAllConfigurations = async () => {
+  const response = await authorizedApi().get("/configuration");
+  return response.data;
+};
+
+export const getConfigurationById = async (id) => {
+  const response = await authorizedApi().get(`/configuration/${id}`);
+  return response.data;
+};
+
+export const createConfiguration = async (data) => {
+  const response = await authorizedApi().post("/configuration", data);
+  return response.data;
+};
+
+export const updateConfiguration = async (data) => {
+  const response = await authorizedApi().put("/configuration/", data);
+  return response.data;
+};
+
+export const deleteConfiguration = async (id) => {
+  const response = await authorizedApi().delete(`/configuration/${id}`);
+  return response.data;
+};
